0bd701aa80db206c15fb954ec45f54b6bbe8ceb5
root <root@localhost.localdomain>
2018-12-05 14:21:09 -0500

    update md5.py

---
+++ md5.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import hashlib
+import sys
+import os
+
+
+def Md5sum(f):
+    md5 = hashlib.md5()
+    if os.path.isdir(f):
+        print 'it is dir'
+    else:
+        f = open(f,'rb')
+        md5.update(f.read())
+        f.close()
+        return md5.hexdigest()
+def Dict_file(Dir):
+    if not os.path.isdir(Dir):
+        print 'please add args for dir'
+    else:
+        file_dict = {}
+        for path,dirs,fs in os.walk(Dir):
+            for f in fs:
+                f_list = []
+                f_path = os.path.join(path,f)
+                f_list.append(f_path)
+                file_dict[f] = f_list
+        return file_dict
+def Md5_dict(Dict):
+    for f in Dict:
+        file_md5 = Md5sum(Dict[f][0])
+        Dict[f].append(file_md5)
+    return Dict
+def Compare_dict(d1,d2):
+    for f in d1:
+        if f in d2:
+            if d1[f][1] != d2[f][1]:
+                print d1[f][0],d1[f][1],'####### md5 != #######',d2[f][0],d2[f][1] 
+        else:
+            print d1[f][0],d1[f][1]
+    for f in d2:
+        if f in d1:
+            pass
+        else:
+            print d2[f][0],d2[f][1]
+if __name__ == '__main__':
+    file_path = sys.argv[1:]
+    try:
+        if '-d' in file_path:
+                file_path.remove('-d')
+                dir1 =  Md5_dict(Dict_file(file_path[0]))
+                dir2 =  Md5_dict(Dict_file(file_path[1]))
+                Compare_dict(dir1,dir2)
+                 
+    finally:
+        for File in file_path:
+            pass
+            #print Md5sum(File),File
+
